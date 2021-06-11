4ba3c8d097fe200e0ddc0c95157aeb1d1df201b2
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2021-06-11 09:00:07 -0400

    few more changes

--- hack-snaps/hack-twitter/index.js
+++ hack-snaps/hack-twitter/index.js
@@ -14,44 +14,32 @@ const provider = new ethers.providers.Web3Provider(wallet);
 wallet.registerRpcMessageHandler(async (_originString, requestObject) => {
   console.log('received request', requestObject);
   const privKey = await wallet.getAppKey();
-  console.log(`privKey is ${privKey}`);
+  // console.log(`privKey is ${privKey}`);
   const ethWallet = new ethers.Wallet(privKey, provider);
-  console.dir(ethWallet);
+  // console.dir(ethWallet);
 
   switch (requestObject.method) {
-    case 'address':
-      return ethWallet.address;
-
     case 'getTwitterVC': {
-      const message = requestObject.params[0];
-      console.log('trying to sign message', message);
-      return ethWallet.signMessage(message);
-      // //get twitter VC
-      // const address = ethWallet.address;
-      // const sig = await ethWallet.signMessage(`twitter access`);        
+      const address = ethWallet.address;
+      const sig = await ethWallet.signMessage(`twitter access`);        
 
-      // console.log("checking for VC...")
-      // const results = await axios.get(`http://localhost:4000/getCredential/${address}/twitter/${sig}`)
-      // console.log(results.data)
+      console.log("checking for VC...")
+      const results = await axios.get(`http://localhost:4000/getCredential/${address}/twitter/${sig}`)
+      console.log(results.data)
 
-      // if (results.data.length>0) {      
-      //   //add method for filtering all unique later, "foreach" isn't required if we can filter straight from api query. 
-      //     results.data.forEach((d) => {
-      //       if(d.verifiableCredential.credentialSubject.id===`did:ethr:${address}`)
-      //       {
-      //         console.log(d.verifiableCredential.credentialSubject.proof.split("/"))
-      //         return ({"account":d.verifiableCredential.credentialSubject.twitterAccount, "link": d.verifiableCredential.credentialSubject.proof.split("/")[5])
-      //       }
-      //     })
-      //   }
-      // } else {
-      //   return({"account": "not found", "link": "not found"})
-      // }
-    }
-
-    case 'sign': {
-      const transaction = requestObject.params[0];
-      return ethWallet.sign(transaction);
+      if (results.data.length>0) {      
+        //add method for filtering all unique later, "foreach" isn't required if we can filter straight from api query. 
+          results.data.forEach((d) => {
+            if(d.verifiableCredential.credentialSubject.id===`did:ethr:${address}`)
+            {
+              console.log(d.verifiableCredential.credentialSubject.proof.split("/"))
+              return ({"account":d.verifiableCredential.credentialSubject.twitterAccount, "link": d.verifiableCredential.credentialSubject.proof.split("/")[5])
+            }
+          })
+        }
+      } else {
+        return({"account": "not found", "link": "not found"})
+      }
     }
 
     default:
